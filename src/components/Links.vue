43702f3dec39bba6fc2fa1fc3aa16f2a4d3905c6
xuanmenliuyao <1601958350@qq.com>
2024-07-20 13:14:51 +0800

    sites

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Safari, CommentsRegular, Markdown, DAndD, YinYang, CloudSun, FolderOpenRegular, Github, PenFancy, Search, Cloudflare, FeatherAlt, MailBulk, Envelope } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Safari, CommentsRegular, FreeCodeCamp, Markdown, DAndD, YinYang, CloudSun, FolderOpenRegular, Github, PenFancy, Search, Cloudflare, FeatherAlt, MailBulk, Envelope } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -69,8 +69,9 @@ const siteIcon = {
   Compass,
   Book,
   Fire,
-  LaptopCode,
+  FreeCodeCamp,
   Markdown,
+  LaptopCode,
   CloudSun,
   Safari,
   CommentsRegular,
